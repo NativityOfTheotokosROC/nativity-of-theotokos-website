d52157921b5a7d033043ef228ba1b65a960a70e7
Grod56 <rodneygaryx29@gmail.com>
2026-01-05 12:10:25 +0200

    time localization

--- src/lib/utility/server-actions.ts
+++ src/lib/utility/server-actions.ts
@@ -2,12 +2,13 @@
 
 import holytrinityorthodox from "../third-party/holytrinityorthodox";
 import mailerLite from "../third-party/mailer-lite";
+import { toZonedTime } from "date-fns-tz";
 
 export async function subscribeToMailingList(email: string) {
 	mailerLite.subscribers.createOrUpdate({ email });
 }
 
 export async function getDailyReadings() {
-	const currentDate = new Date();
-	return holytrinityorthodox.getDailyReadings(currentDate);
+	const localDate = toZonedTime(new Date(), "CAT");
+	return holytrinityorthodox.getDailyReadings(localDate);
 }
