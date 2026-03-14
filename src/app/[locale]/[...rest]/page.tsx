b5f1e8932350b369139308b29c54880488585f8f
Garikai Gumbo <rodneygaryx29@gmail.com>
2026-03-14 10:17:13 +0200

    hot-fix

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -3,6 +3,9 @@ import { setRequestLocale } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "@/src/i18n/routing";
 
+export function generateStaticParams() {
+	return routing.locales.map((locale) => ({locale}));
+}
 
 export default async function CatchAll({ params }: LayoutProps<"/[locale]">) {
 	const { locale } = await params;
