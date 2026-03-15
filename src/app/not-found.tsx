4adf53fd2f39f90317ca7b78426c5841e84dc3ca
Grod56 <rodneygaryx29@gmail.com>
2026-03-15 23:28:42 +0200

    fixed cacheComponents integration

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,26 +2,31 @@ import "./globals.css";
 
 import { NextIntlClientProvider } from "next-intl";
 import NotFoundPage from "../lib/component/pages/not-found/NotFound";
-import { Google_Sans_Flex } from "next/font/google";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import { googleSansFlex } from "../lib/third-party/fonts";
+import { setRequestLocale } from "next-intl/server";
+import { routing } from "../i18n/routing";
 
-const googleSansFlex = Google_Sans_Flex({
-	subsets: ["latin"],
-	variable: "--font-g-flex",
-	display: "swap",
-});
+export function generateStaticParams() {
+	return routing.locales.map(locale => ({ locale }));
+}
 
 export const metadata: Metadata = {
 	title: "Resource not Found",
 };
 
 export default function NotFound() {
+	setRequestLocale("en");
+
 	return (
 		<html data-scroll-behavior="smooth">
 			<body className={`antialiased ${googleSansFlex.className}`}>
-				<NextIntlClientProvider>
-					<NotFoundPage />
-				</NextIntlClientProvider>
+				<Suspense fallback={null}>
+					<NextIntlClientProvider>
+						<NotFoundPage />
+					</NextIntlClientProvider>
+				</Suspense>
 			</body>
 		</html>
 	);
