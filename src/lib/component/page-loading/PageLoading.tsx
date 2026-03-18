d688c373bc52e8d047d1154c8280436b707a3d7e
Grod56 <rodneygaryx29@gmail.com>
2026-03-18 23:26:36 +0200

    minor qol updates

--- src/lib/component/page-loading/PageLoading.tsx
+++ src/lib/component/page-loading/PageLoading.tsx
@@ -4,7 +4,7 @@ import { LogoIcon } from "../miscellaneous/graphic";
 
 export default function PageLoading() {
 	return (
-		<div className="page-loading flex flex-col justify-center items-center h-fit min-h-[78lvh] w-full bg-[#fef8f3]">
+		<div className="page-loading flex flex-col justify-center items-center h-fit min-h-[82vh] w-full bg-[#fef8f3]">
 			<div
 				className={`flex gap-3 items-center justify-center max-w-[25em] animate-pulse`}
 			>
