4337a8e789afa8624cb03656871b39bc28d20aa0
Grod56 <rodneygaryx29@gmail.com>
2026-03-23 03:05:41 +0200

    modified usernavigationwidget for navbar

--- src/lib/component/user-navigation-widget/UserNavigationWidget.tsx
+++ src/lib/component/user-navigation-widget/UserNavigationWidget.tsx
@@ -3,32 +3,45 @@ import { ChevronDown as DropdownIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { UserNavigationWidgetModel } from "../../model/user-navigation-widget";
+import { InitializedModel } from "@mvc-react/mvc";
 
 const UserNavigationWidget = function ({ model }) {
-	const { modelView } = model;
+	const {
+		modelView: { userDetails, type },
+	} = model;
 
 	const t = useTranslations("userNavigation");
+	const greeting =
+		userDetails && `${t("greeting")}, ${userDetails.name.split(" ")[0]}`;
 
-	return modelView?.userDetails ? (
+	return userDetails ? (
 		<div className="user-navigation">
-			<div className="user-navigation-content bg-gray-800 flex flex-col">
-				<button className="user-button flex justify-between items-center text-left gap-3 px-4 py-4 text-white">
+			<div
+				className={`user-navigation-content ${type == "sidebar" && "bg-gray-800"} flex flex-col`}
+			>
+				<button
+					className={`user-button flex justify-between items-center text-left gap-3 ${type == "sidebar" && "px-4 py-4"} text-white`}
+					title={type == "navbar" && greeting ? greeting : undefined}
+				>
 					<div className="user-pane w-full flex gap-3 items-center">
-						<div className="avatar-frame flex items-stretch justify-stretch size-10 min-w-10 rounded-sm overflow-clip border-2 border-white/15">
+						<div className="avatar-frame flex items-stretch justify-stretch size-10 min-w-10 rounded-sm overflow-clip border border-white/15">
 							<Image
 								className="grow object-center object-cover"
 								alt={
-									modelView.userDetails.avatar.about ??
-									modelView.userDetails.name
+									userDetails.avatar.about ?? userDetails.name
 								}
-								width={20}
-								height={20}
-								src={modelView.userDetails.avatar.source}
+								width={40}
+								height={40}
+								src={userDetails.avatar.source}
 							/>
 						</div>
-						<span
-							className={`greeting max-w-30 wrap-break-word hyphens-auto`}
-						>{`${t("greeting")}, ${modelView.userDetails.name.split(" ")[0]}`}</span>
+						{type == "sidebar" && (
+							<span
+								className={`greeting max-w-30 wrap-break-word hyphens-auto`}
+							>
+								{greeting}
+							</span>
+						)}
 					</div>
 					<DropdownIcon className="min-w-fit" strokeWidth={1.75} />
 				</button>
@@ -37,6 +50,6 @@ const UserNavigationWidget = function ({ model }) {
 	) : (
 		<></>
 	);
-} satisfies ModeledVoidComponent<UserNavigationWidgetModel>;
+} satisfies ModeledVoidComponent<InitializedModel<UserNavigationWidgetModel>>;
 
 export default UserNavigationWidget;
