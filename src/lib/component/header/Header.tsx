a789c9040d417d239d3f3285451521c8c3692976
Nativity of Theotokos Russian Orthodox Church <nativityoftheotokos@gmail.com>
2026-03-25 19:07:15 +0200

    Header.tsx hotfix

--- src/lib/component/header/Header.tsx
+++ src/lib/component/header/Header.tsx
@@ -37,7 +37,7 @@ const Header = function ({ model }) {
 						name: userDetails.name,
 						avatar: userDetails.avatar,
 					},
-					userActions,
+					userActions: userActions!,
 				}
 			: null,
 	});
