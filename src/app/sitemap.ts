915e1140f69570bf2e24c02644060e0eb985e80a
Grod56 <rodneygaryx29@gmail.com>
2026-03-28 10:31:59 +0200

    sitemap fix attempt 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,10 @@ import type { MetadataRoute } from "next";
 import { getAllArticles } from "../lib/server-action/news-article";
 import { getBaseURL } from "../lib/server-action/miscellaneous";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const dynamicParams = true;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const baseUrl = await getBaseURL();
 	const newsArticles = await getAllArticles();
