69c14964013f3ebfe9feac322ff8d13eca04a211
Grod56 <rodneygaryx29@gmail.com>
2026-03-28 11:31:28 +0200

    sitemap fix attempt 4

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,18 +2,20 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 import { NextRequest, NextResponse } from "next/server";
 
+const nextIntlMiddleware = createMiddleware(routing);
+
 export default function middleware(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 
 	if (pathname.startsWith("/sitemap") || pathname.startsWith("/robots"))
 		return NextResponse.next();
 
-	return createMiddleware(routing)(req);
+	return nextIntlMiddleware(req);
 }
 
 export const config = {
 	// Match all pathnames except for
 	// - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
 	// - … the ones containing a dot (e.g. `favicon.ico`)
-	matcher: "/((?!api|trpc|_next|_vercel|.*\\..*|sitemap.xml|robots.txt).*)",
+	matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
