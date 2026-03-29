1bbca1beb17b5c590bc07e9bf30508e2f23f764b
Grod56 <rodneygaryx29@gmail.com>
2026-03-29 09:51:33 +0200

    sitemap fix attempt 8

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,5 +15,5 @@ export default function middleware(req: NextRequest) {
 
 export const config = {
 	// idk
-	matcher: ["/", `(${routing.locales.join("|")}):path*`],
+	matcher: ["/", `/(en|ru)/:path*`],
 };
