045ffe74a15abff3d7f8d9d37c9dc6676808bc4b
Grod56 <rodneygaryx29@gmail.com>
2026-03-29 09:45:55 +0200

    sitemap fix attempt 8

--- src/proxy.ts
+++ src/proxy.ts
@@ -14,8 +14,6 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-	// Match all pathnames except for
-	// - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-	// - … the ones containing a dot (e.g. `favicon.ico`)
-	matcher: "/((?!api|trpc|_next|_vercel|sitemap.xml|.*\\..*).*)",
+	// idk
+	matcher: ["/", `(${routing.locales.join("|")}):path*`],
 };
