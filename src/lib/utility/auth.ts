3c8189280b4e677f5bf514bf8d38186331a052f9
Grod56 <rodneygaryx29@gmail.com>
2026-04-01 16:25:03 +0200

    investigating form submission bug

--- src/lib/utility/auth.ts
+++ src/lib/utility/auth.ts
@@ -1,5 +1,9 @@
 export function getProtectedRoutes() {
-	const protectedRoutes = ["/quotes/new", "/admin"] as const;
+	const environment = process.env.VERCEL_ENV ?? process.env.NODE_ENV;
+	const protectedRoutes =
+		environment == "production"
+			? (["/quotes/new", "/admin"] as const)
+			: ([] as const);
 	// This should do for now lol
 	type TypeVerify =
 		PageProps<`/[locale]${(typeof protectedRoutes)[number]}`> extends never
