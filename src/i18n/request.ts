42ef931345ad64c36cac6914d8ccd84ee70f337e
Grod56 <rodneygaryx29@gmail.com>
2026-04-09 11:30:30 +0200

    trying out rootParams + added share button

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,7 @@
+import * as rootParams from "next/root-params";
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
-import * as rootParams from "next/root-params";
 import { notFound } from "next/navigation";
 
 export default getRequestConfig(async ({ locale }) => {
