d9f911f71431d918b1d1a523479cd48d6afa1a16
Grod56 <rodneygaryx29@gmail.com>
2026-04-09 21:02:40 +0200

    testing out global-not-found

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -5,21 +5,18 @@ import { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Suspense } from "react";
-import { routing } from "../i18n/routing";
 import NotFoundPage from "../lib/component/page/not-found/NotFound";
 import ViewLoadingSkeleton from "../lib/component/view-loading-skeleton/ViewLoadingSkeleton";
 import ClientProviders from "../lib/provider/client-providers";
 import { googleSansFlex } from "../lib/third-party/fonts";
-
-export function generateStaticParams() {
-	return routing.locales.map(locale => ({ locale }));
-}
 
 export const metadata: Metadata = {
 	title: "404 - Resource not Found",
 };
 
 export default async function NotFound() {
+	"use cache";
+
 	const language = "en";
 	const messages = await getMessages({ locale: language });
 
