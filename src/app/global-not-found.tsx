dae94ecb502336d832a83bf8a74ad3285b2dc24e
Grod56 <rodneygaryx29@gmail.com>
2026-04-09 20:45:01 +0200

    testing out global-not-found

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -2,10 +2,13 @@ import "./globals.css";
 
 import { newReadonlyModel } from "@mvc-react/mvc";
 import { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import { Suspense } from "react";
 import { routing } from "../i18n/routing";
 import NotFoundPage from "../lib/component/page/not-found/NotFound";
 import ViewLoadingSkeleton from "../lib/component/view-loading-skeleton/ViewLoadingSkeleton";
+import ClientProviders from "../lib/provider/client-providers";
 import { googleSansFlex } from "../lib/third-party/fonts";
 
 export function generateStaticParams() {
@@ -16,13 +19,24 @@ export const metadata: Metadata = {
 	title: "404 - Resource not Found",
 };
 
-export default function NotFound() {
+export default async function NotFound() {
 	const language = "en";
+	const messages = await getMessages({ locale: language });
+
 	return (
 		<html lang={language} data-scroll-behavior="smooth">
 			<body className={`antialiased ${googleSansFlex.className}`}>
 				<Suspense fallback={<ViewLoadingSkeleton />}>
-					<NotFoundPage model={newReadonlyModel({ language })} />
+					<NextIntlClientProvider
+						locale={language}
+						messages={messages}
+					>
+						<ClientProviders>
+							<NotFoundPage
+								model={newReadonlyModel({ language })}
+							/>
+						</ClientProviders>
+					</NextIntlClientProvider>
 				</Suspense>
 			</body>
 		</html>
