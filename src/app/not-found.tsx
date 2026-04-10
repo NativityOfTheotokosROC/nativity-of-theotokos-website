60f10452416f2347976bd010078dc22ce86e19f4
Garikai Gumbo <rodneygaryx29@gmail.com>
2026-04-10 12:32:43 +0200

    trying root not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,40 @@
+import "./globals.css";
+
+import { newReadonlyModel } from "@mvc-react/mvc";
+import { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages, setRequestLocale } from "next-intl/server";
+import { Suspense } from "react";
+import NotFoundPage from "../lib/component/page/not-found/NotFound";
+import ViewLoadingSkeleton from "../lib/component/view-loading-skeleton/ViewLoadingSkeleton";
+import ClientProviders from "../lib/provider/client-providers";
+import { googleSansFlex } from "../lib/third-party/fonts";
+
+export const metadata: Metadata = {
+	title: "404 - Resource not Found",
+};
+
+export default async function NotFound() {
+	"use cache";
+
+	const language = "en";
+	setRequestLocale(language);
+	const messages = await getMessages({ locale: language });
+
+	return (
+		<html lang={language} data-scroll-behavior="smooth">
+			<body className={`antialiased ${googleSansFlex.className}`}>
+				<Suspense fallback={<ViewLoadingSkeleton />}>
+					<NextIntlClientProvider
+						locale={language}
+						messages={messages}
+					>
+						<NotFoundPage
+							model={newReadonlyModel({ language })}
+						/>
+					</NextIntlClientProvider>
+				</Suspense>
+			</body>
+		</html>
+	);
+}
