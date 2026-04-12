6d89f61d21464bfd9f22d833acb5d6d538c1c25b
Grod56 <rodneygaryx29@gmail.com>
2026-04-12 05:24:01 +0200

    news article not found bug fix (frfrfrfr)

--- src/app/[locale]/news/[article]/page.tsx
+++ src/app/[locale]/news/[article]/page.tsx
@@ -50,13 +50,13 @@ export async function generateMetadata({
 			description: snippet,
 			// url: `/news/${uri}`,
 			type: "article",
-			images: [{ url: articleImage.source }],
+			images: [articleImage.source],
 		},
 		twitter: {
 			card: "summary_large_image",
 			title,
 			description: snippet,
-			images: [{ url: articleImage.source }],
+			images: [articleImage.source],
 		},
 	};
 }
