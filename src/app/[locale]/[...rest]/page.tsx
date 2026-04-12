d246e2bd757cad7e1b45412405d567b220b73acb
Grod56 <rodneygaryx29@gmail.com>
2026-04-12 11:24:58 +0200

    [...rest] generateMetadata attempted fix

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,6 +1,6 @@
 import { Language } from "@/src/lib/type/general";
 import { Metadata } from "next";
-import { getTranslations } from "next-intl/server";
+import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { locale as rootLocale } from "next/root-params";
 
@@ -8,6 +8,7 @@ export async function generateMetadata(): Promise<Metadata> {
 	"use cache";
 
 	const { locale } = await rootLocale();
+	setRequestLocale(locale);
 
 	const t = await getTranslations({
 		locale: locale as Language,
