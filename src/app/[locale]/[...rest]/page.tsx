3834f5f8af463ec31728e591bbca7bf2f71d8079
Grod56 <rodneygaryx29@gmail.com>
2026-04-12 11:19:09 +0200

    layout generateMetadata attempted fix

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,4 +1,21 @@
+import { Language } from "@/src/lib/type/general";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
+import { locale as rootLocale } from "next/root-params";
+
+export async function generateMetadata(): Promise<Metadata> {
+	"use cache";
+
+	const { locale } = await rootLocale();
+
+	const t = await getTranslations({
+		locale: locale as Language,
+		namespace: "notFound",
+	});
+
+	return { title: t("metaTitle") };
+}
 
 export default async function Page() {
 	notFound();
