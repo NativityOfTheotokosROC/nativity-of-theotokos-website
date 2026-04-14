afd4ac850de0acd5b05484f3e37b816bd46687b7
Grod56 <rodneygaryx29@gmail.com>
2026-04-14 02:02:50 +0200

    home usequery bug fix

--- src/lib/model-implementation/home.ts
+++ src/lib/model-implementation/home.ts
@@ -1,44 +1,41 @@
-import { useNewStatefulInteractiveModel } from "@mvc-react/stateful";
 import { useQuery } from "@tanstack/react-query";
 import { useLocale } from "next-intl";
-import { HomeModel, HomeModelInteraction, HomeModelView } from "../model/home";
+import { HomeModel, HomeModelInteraction } from "../model/home";
 import { getHomeSnapshot } from "../server-action/home";
 
 export function useHome(): HomeModel {
 	const language = useLocale();
-	const { modelView, interact } = useNewStatefulInteractiveModel<
-		HomeModelView,
-		HomeModelInteraction
-	>({
-		async produceModelView(interaction) {
-			switch (interaction.type) {
-				case "REFRESH": {
-					const {
-						dailyReadings,
-						dailyQuote,
-						scheduleItems,
-						newsArticles,
-						dailyGalleryImages,
-					} = await getHomeSnapshot(4, 4, 7, language);
-					return {
-						dailyReadings,
-						dailyQuote,
-						scheduleItems,
-						newsArticles,
-						dailyGalleryImages,
-					};
-				}
-			}
-		},
-	});
-	useQuery({
+	const { data, isSuccess, refetch } = useQuery({
 		queryKey: ["home"],
-		queryFn: () => interact({ type: "REFRESH" }),
+		queryFn: () => getHomeSnapshot(4, 4, 7, language),
 		staleTime: Infinity,
 		gcTime: Infinity,
 		refetchOnMount: false,
 		refetchOnReconnect: false,
 		refetchOnWindowFocus: false,
 	});
-	return { modelView, interact };
+	const interact = async (interaction: HomeModelInteraction) => {
+		if (interaction.type == "REFRESH") refetch();
+	};
+
+	if (isSuccess) {
+		const {
+			dailyGalleryImages,
+			dailyQuote,
+			dailyReadings,
+			newsArticles,
+			scheduleItems,
+		} = data;
+		return {
+			modelView: {
+				dailyGalleryImages,
+				dailyQuote,
+				dailyReadings,
+				newsArticles,
+				scheduleItems,
+			},
+			interact,
+		};
+	}
+	return { modelView: null, interact };
 }
