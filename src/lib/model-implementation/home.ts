61ab85254b474897ea311a34429cc31b1670ba34
Grod56 <rodneygaryx29@gmail.com>
2026-04-14 22:48:23 +0200

    reverted to old fetching strategy for home

--- src/lib/model-implementation/home.ts
+++ src/lib/model-implementation/home.ts
@@ -1,41 +1,76 @@
-import { useQuery } from "@tanstack/react-query";
+import { useNewStatefulInteractiveModel } from "@mvc-react/stateful";
 import { useLocale } from "next-intl";
-import { HomeModel, HomeModelInteraction } from "../model/home";
+import { useEffect } from "react";
+import { HomeModel, HomeModelInteraction, HomeModelView } from "../model/home";
 import { getHomeSnapshot } from "../server-action/home";
 
 export function useHome(): HomeModel {
 	const language = useLocale();
-	const { data, isSuccess, refetch } = useQuery({
-		queryKey: ["home"],
-		queryFn: () => getHomeSnapshot(4, 4, 7, language),
-		staleTime: Infinity,
-		gcTime: Infinity,
-		refetchOnMount: false,
-		refetchOnReconnect: false,
-		refetchOnWindowFocus: false,
+	const { modelView, interact } = useNewStatefulInteractiveModel<
+		HomeModelView,
+		HomeModelInteraction
+	>({
+		async produceModelView(interaction) {
+			switch (interaction.type) {
+				case "REFRESH": {
+					const {
+						dailyReadings,
+						dailyQuote,
+						scheduleItems,
+						newsArticles,
+						dailyGalleryImages,
+					} = await getHomeSnapshot(4, 4, 7, language);
+					return {
+						dailyReadings,
+						dailyQuote,
+						scheduleItems,
+						newsArticles,
+						dailyGalleryImages,
+					};
+				}
+			}
+		},
 	});
-	const interact = async (interaction: HomeModelInteraction) => {
-		if (interaction.type == "REFRESH") refetch();
-	};
 
-	if (isSuccess) {
-		const {
-			dailyGalleryImages,
-			dailyQuote,
-			dailyReadings,
-			newsArticles,
-			scheduleItems,
-		} = data;
-		return {
-			modelView: {
-				dailyGalleryImages,
-				dailyQuote,
-				dailyReadings,
-				newsArticles,
-				scheduleItems,
-			},
-			interact,
-		};
-	}
-	return { modelView: null, interact };
+	useEffect(() => {
+		if (!modelView) {
+			interact({ type: "REFRESH" });
+		}
+	}, [modelView, interact]);
+	return { modelView, interact };
+
+	// TODO: Reinstate once Error 310 cause is resolved
+	// const { data, isSuccess, refetch } = useQuery({
+	// 	queryKey: ["home"],
+	// 	queryFn: () => getHomeSnapshot(4, 4, 7, language),
+	// 	staleTime: Infinity,
+	// 	gcTime: Infinity,
+	// 	refetchOnMount: false,
+	// 	refetchOnReconnect: false,
+	// 	refetchOnWindowFocus: false,
+	// });
+	// const interact = async (interaction: HomeModelInteraction) => {
+	// 	if (interaction.type == "REFRESH") refetch();
+	// };
+
+	// if (isSuccess) {
+	// 	const {
+	// 		dailyGalleryImages,
+	// 		dailyQuote,
+	// 		dailyReadings,
+	// 		newsArticles,
+	// 		scheduleItems,
+	// 	} = data;
+	// 	return {
+	// 		modelView: {
+	// 			dailyGalleryImages,
+	// 			dailyQuote,
+	// 			dailyReadings,
+	// 			newsArticles,
+	// 			scheduleItems,
+	// 		},
+	// 		interact,
+	// 	};
+	// }
+	// return { modelView: null, interact };
 }
