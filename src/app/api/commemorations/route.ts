8c940b8feae026b8f8bfaf46593a20026b752cc4
Grod56 <rodneygaryx29@gmail.com>
2026-04-24 13:36:14 +0200

    commemorations cron job

--- src/app/api/commemorations/route.ts
+++ src/app/api/commemorations/route.ts
@@ -1,22 +1,16 @@
 import { getSaints } from "@/src/lib/third-party/holytrinityorthodox";
 import prisma from "@/src/lib/third-party/prisma";
-import { getLocalTimeZone } from "@/src/lib/utilities/date-time";
 import { load } from "cheerio";
 import { addDays } from "date-fns";
-import { toZonedTime } from "date-fns-tz";
 import { backOff } from "exponential-backoff";
 
 export async function GET(request: Request) {
-	const startDate = toZonedTime(
-		new Date(new Date().getFullYear(), 0, 1),
-		getLocalTimeZone(),
-	);
+	const startDate = new Date(new Date().getFullYear(), 0, 1);
 	let nextDate = startDate;
 	const ids = new Set<string>();
-	console.log(nextDate);
-	console.log(startDate);
 	while (nextDate.getFullYear() === startDate.getFullYear()) {
 		const saints = await backOff(() => getSaints(nextDate, "en"));
+		console.log(saints);
 		const $ = load(saints);
 		$("a").each(function () {
 			const pathParts = $(this).attr("href")!.split("/");
