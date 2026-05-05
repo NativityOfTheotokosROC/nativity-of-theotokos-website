710f4b4908e392fa7e832a2a4b8a7efca12d0204
Grod56 <rodneygaryx29@gmail.com>
2026-05-06 00:06:02 +0200

    added yandex plugin

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { genericOAuth } from "better-auth/plugins";
+import { getYandexOAuthPlugin } from "@better-auth-extensions/yandex";
 import prisma from "./src/lib/third-party/prisma";
 import {
 	BETTER_AUTH_URL,
@@ -31,35 +32,7 @@ export const auth = betterAuth({
 	plugins: [
 		genericOAuth({
 			config: [
-				{
-					providerId: "yandex",
-					clientId: YANDEX_CLIENT_ID,
-					clientSecret: YANDEX_CLIENT_SECRET,
-					authorizationUrl: "https://oauth.yandex.com/authorize",
-					authorizationUrlParams: {
-						scope: "login:email login:info login:avatar",
-					},
-					tokenUrl: "https://oauth.yandex.com/token",
-					userInfoUrl: "https://login.yandex.ru/info?format=json",
-					mapProfileToUser(profile) {
-						return {
-							id: profile.id,
-							name:
-								profile.display_name ??
-								profile.real_name ??
-								profile.first_name,
-							email:
-								profile.default_email ??
-								profile.emails?.[0] ??
-								null,
-							image:
-								!profile.is_avatar_empty &&
-								profile.default_avatar_id
-									? `https://avatars.yandex.net/get-yapic/${profile.default_avatar_id}/islands-200`
-									: null,
-						};
-					},
-				},
+				getYandexOAuthPlugin(YANDEX_CLIENT_ID, YANDEX_CLIENT_SECRET),
 			],
 		}),
 	],
