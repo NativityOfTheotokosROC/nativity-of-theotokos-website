e1e4bdf9552e764acdbae6d94a4d2bb7818c454f
Garikai Gumbo <rodneygaryx29@gmail.com>
2026-09-26 06:01:56 +0200

    Update AutoCompleteBox.tsx

--- src/lib/components/auto-complete-box/AutoCompleteBox.tsx
+++ src/lib/components/auto-complete-box/AutoCompleteBox.tsx
@@ -1,6 +1,6 @@
 import { InitializedModel } from "@mvc-react/mvc";
-import { useEffect, useRef, useState } from "react";
-import { Tooltip, TooltipRefProps } from "react-tooltip";
+import { useState } from "react";
+import { Tooltip } from "react-tooltip";
 import { AutoCompleteBoxModel } from "../../models/auto-complete-box";
 import "./auto-complete-box.css";
 
@@ -23,7 +23,7 @@ export default function AutoCompleteBox<I, K extends string>({
 	const [isClickable, setClickable] = useState(computedOpen); //TODO: Not ideal
 	const [lastVisibleItems, setLastVisibleItems] = useState(filteredItems);
 	const displayedItems = computedOpen ? filteredItems : lastVisibleItems;
-	const tooltipRef = useRef<TooltipRefProps>(null);
+	
 	if (
 		computedOpen &&
 		JSON.stringify(filteredItems) !== JSON.stringify(lastVisibleItems)
@@ -31,24 +31,16 @@ export default function AutoCompleteBox<I, K extends string>({
 		setLastVisibleItems(filteredItems);
 	}
 
-	useEffect(() => {
-		if (computedOpen) {
-			tooltipRef.current?.open({
-				anchorSelect: `#${id}`,
-			});
-		} else {
-			tooltipRef.current?.close();
-		}
-	}, [computedOpen, id]);
-
 	return (
 		<Tooltip
 			className="auto-complete-box bg-gray-950"
+			anchorSelect={`#${id}`}
+			key={id}
+			isOpen={computedOpen}
 			clickable={isClickable}
 			afterHide={() => setClickable(false)}
 			afterShow={() => setClickable(true)}
 			place="bottom-start"
-			ref={tooltipRef}
 			content={
 				<div className="auto-complete-items flex max-h-[9em] w-[17em] max-w-[17em] flex-col overflow-y-auto pr-3 text-sm">
 					{displayedItems.map(({ item, arrayIndex }) => (
